35bcbb8a563762d692c5a1622922d40981047849
EnzoHamblotMarquez <ehamblotm@edu.tecnocampus.cat>
2026-02-28 17:13:39 +0100

    Stat Dictionary

--- Assets/Scripts/Manager/TestManager.cs
+++ Assets/Scripts/Manager/TestManager.cs
@@ -1,5 +1,6 @@
 using UnityEngine;
 using System.Collections.Generic;
+using System.Linq;
 
 public class TestManager : MonoBehaviour
 {
@@ -7,11 +8,15 @@ public class TestManager : MonoBehaviour
     public CardSpawner spawner;
     public Fade UI;
 
+    [Header("Win / Lose onditions")]
+    public bool LoseOnAnyZero = true;
+
     [Header("Stats")]
-    public float StatA; //!
-    public float StatB; //!
-    public float StatC; //!
-    public float StatD; //!
+    //public float StatA; //!
+    //public float StatB; //!
+    //public float StatC; //!
+    //public float StatD; //!
+    private Dictionary<string, float> stats = new Dictionary<string, float>();
 
     public float maxForStat = 100.0f;
     public float statInitialValue = 25.0f;
@@ -40,7 +45,6 @@ public class TestManager : MonoBehaviour
         {
             instance = this;
             DontDestroyOnLoad(gameObject);
-            InitializeStats();
         }
         else
             Destroy(gameObject);
@@ -48,21 +52,24 @@ public class TestManager : MonoBehaviour
 
     private void Start()
     {
+        InitializeStats();
         GoToNextDay();
     }
 
     void InitializeStats()
     {
-        StatA = statInitialValue;
-        StatB = statInitialValue;
-        StatC = statInitialValue;
-        StatD = statInitialValue;
+        stats.Add("StatA", statInitialValue);
+        stats.Add("StatB", statInitialValue);
+        stats.Add("StatC", statInitialValue);
+        stats.Add("StatD", statInitialValue);
+        //StatA = statInitialValue;
+        //StatB = statInitialValue;
+        //StatC = statInitialValue;
+        //StatD = statInitialValue;
     }
 
     private void Update()
     {
-
-
         if (!startDay)
         {
             if (Input.GetKeyDown(KeyCode.Space))
@@ -74,10 +81,14 @@ public class TestManager : MonoBehaviour
 
         if (DayTimerProgress)
         {
-            StatA -= Time.deltaTime;
-            StatB -= Time.deltaTime;
-            StatC -= Time.deltaTime;
-            StatD -= Time.deltaTime;
+            foreach (var key in stats.Keys)
+            {
+                stats[key] -= Time.deltaTime;
+            }
+            //StatA -= Time.deltaTime;
+            //StatB -= Time.deltaTime;
+            //StatC -= Time.deltaTime;
+            //StatD -= Time.deltaTime;
 
             ClampStats();
 
@@ -90,16 +101,20 @@ public class TestManager : MonoBehaviour
         }
 
 
-
+        if (CheckGameOverConditions()) Debug.Log("GAME OVER");
 
     }
 
     void ClampStats()
     {
-        StatA = Mathf.Clamp(StatA, 0.0f, maxForStat);
-        StatB = Mathf.Clamp(StatB, 0.0f, maxForStat);
-        StatC = Mathf.Clamp(StatC, 0.0f, maxForStat);
-        StatD = Mathf.Clamp(StatD, 0.0f, maxForStat);
+        foreach (var key in stats.Keys.ToList())
+        {
+            stats[key] = Mathf.Clamp(stats[key], 0.0f, maxForStat);
+        }
+        //StatA = Mathf.Clamp(StatA, 0.0f, maxForStat);
+        //StatB = Mathf.Clamp(StatB, 0.0f, maxForStat);
+        //StatC = Mathf.Clamp(StatC, 0.0f, maxForStat);
+        //StatD = Mathf.Clamp(StatD, 0.0f, maxForStat);
     }
 
     public void GetNextCard()
@@ -117,16 +132,20 @@ public class TestManager : MonoBehaviour
             switch (mod.StatType)
             {
                 case StatType.StatA:
-                    StatA += mod.amount;
+                    stats["StatA"] += mod.amount;
+                    //StatA += mod.amount;
                     break;
                 case StatType.StatB:
-                    StatB += mod.amount;
+                    stats["StatB"] += mod.amount;
+                    //StatB += mod.amount;
                     break;
                 case StatType.StatC:
-                    StatC += mod.amount;
+                    stats["StatC"] += mod.amount;
+                    //StatC += mod.amount;
                     break;
                 case StatType.StatD:
-                    StatD += mod.amount;
+                    stats["StatD"] += mod.amount;
+                    //StatD += mod.amount;
                     break;
             }
         }
@@ -173,8 +192,8 @@ public class TestManager : MonoBehaviour
         //Debug.Log("GoToNextDayWithFade called");
         ResetProgressValues();
 
-        if(!DayTimerProgress) spawner.currentCard.RemoveCard();
-        
+        if (!DayTimerProgress) spawner.currentCard.RemoveCard();
+
         UI.FadeIn(() =>
         {
             GoToNextDay();
@@ -189,5 +208,23 @@ public class TestManager : MonoBehaviour
     {
         if (DayTimerProgress) dayTimer = 0.0f;
         else cardCounter = 0;
+    }
+
+    public bool CheckGameOverConditions()
+    {
+        if (LoseOnAnyZero)
+        {
+            foreach (var value in stats.Values)
+            {
+                if (value <= 0.0f) return true;
+            }
+        }
+
+        //if (StatA == 0) return true;
+        //if (StatB == 0) return true;
+        //if (StatC == 0) return true;
+        //if (StatD == 0) return true;
+
+        return false;
     }
 }
